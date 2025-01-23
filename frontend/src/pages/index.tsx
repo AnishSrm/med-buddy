d1489a070b5e4409d797d1d1d39c755f240611b8
ANISH MOHAN R R <ak5536@srmist.edu.in>
2025-01-23 13:44:39 +0530

    com

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -52,11 +52,11 @@ export default function Component() {
           <div className="flex gap-24 justify-evenly mt-36">
             <div>
               <Image
-                src="/anish.JPG"
-                alt=""
+                src="/med-buddy/frontend/public/anish.jpg"
+                alt="Description"
                 className=""
-                width={500}
-                height={500}
+                width={350}
+                height={350}
               />
               <p className="text-[#3c84fb] text-xl font-semibold mt-4"> Shashank Kumar - RA2011026010181</p>
               <p className="text-[#3c84fb] text-xl font-semibold">CSE with AIML (CINTEL)</p>
